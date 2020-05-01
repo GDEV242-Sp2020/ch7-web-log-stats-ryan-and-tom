2e9e956c63232d4bab624461f7c72e6b267a78fa
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-05-01 14:44:48 -0400

    added methods
    7.12 - 7.16

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,3 +1,4 @@
+
 /**
  * Read web server data and analyse hourly access patterns.
  * 
@@ -21,8 +22,21 @@ public class LogAnalyzer
         hourCounts = new int[24];
         // Create the reader to obtain the data.
         reader = new LogfileReader();
+        
     }
-
+    /**
+     * Create an object to analyze hourly web accesses.
+     * @param file takes in a string file name
+     */
+    public LogAnalyzer(String file)
+    { 
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader(file);
+        
+    }
     /**
      * Analyze the hourly access data from the log file.
      */
@@ -55,4 +69,71 @@ public class LogAnalyzer
     {
         reader.printData();
     }
+    /**
+     * Return the number of accesses recorded in the log file.
+     */
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        for(int i = 0; i < hourCounts.length; i++)
+        {
+            total += hourCounts[i];
+        }
+        
+        return total;
+    }
+    /**
+     * returns the busiest hour
+     * 
+     */
+    public int busiestHour()
+    {
+        int busyHour = 0;
+        int busyStorage = 0;
+        
+        for( int i = 0; i < hourCounts.length; i++)
+        {
+            if(hourCounts[i] > busyStorage)
+            {
+                busyStorage = hourCounts[i];
+                busyHour = i;
+            }
+            else if(hourCounts[i] == busyStorage)
+            {
+                busyHour = i;
+            }
+            
+        }
+        
+        return busyHour;
+    }
+    /**
+     * returns the quiest hour
+     */
+    public int quiestHour()
+    {
+        int quietHour = 0;
+        int quietStorage = hourCounts[0];
+        
+        for (int i = 0; i < hourCounts.length; i++)
+        {
+            if (hourCounts[i] < quietStorage)
+            {
+                quietStorage = hourCounts[i];
+                quietHour = i;
+            }
+            else if(hourCounts[i] == quietStorage)
+            {
+                quietHour = i;
+            }
+        }
+        
+        return quietHour;
+    }
+    
+    
+    
+    
+    
+    
 }
